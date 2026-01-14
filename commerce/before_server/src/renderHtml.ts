4fd8f44597e75644bbad6515d242c467f9708e18
oauth-network <127634415+oauth-network@users.noreply.github.com>
2026-01-15 08:38:21 +0900

    Update

---
+++ commerce/before_server/src/renderHtml.ts
@@ -0,0 +1,15 @@
+export function renderHtml(cookies, rows) {
+	return `
+		<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+				<title>Logis.center</title>
+			</head>
+			<body>
+				<script src="/static/js/content.js"></script>
+			</body>
+		</html>
+	`;
+}
