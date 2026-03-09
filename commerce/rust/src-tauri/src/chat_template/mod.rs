6dd4e532d755b05f62dcafa23205e36b1d6800fc
oauth-network <127634415+oauth-network@users.noreply.github.com>
2026-03-09 17:49:03 +0900

    Update mod.rs

--- commerce/rust/src-tauri/src/chat_template/mod.rs
+++ commerce/rust/src-tauri/src/chat_template/mod.rs
@@ -21,14 +21,8 @@ pub fn get_template(path: String) -> Result<String> {
         .ok_or(anyhow!(format!("chat_template to str error")))?;
     
     let fixed_template = chat_template
-        .replace(
-            "message.content.startswith('<tool_response>')",
-            "message.content is startingwith('<tool_response>')", 
-        )
-        .replace(
-            "message.content.endswith('</tool_response>')",
-            "message.content is endingwith('</tool_response>')", 
-        )
+        .replace(".startswith(", " is startingwith(")
+        .replace(".endswith(", " is endingwith(")
         .replace(
             "content.split('</think>')[0].rstrip('\n').split('<think>')[-1].lstrip('\n')",
             "((content | split('</think>'))[0] | rstrip('\n') | split('<think>'))[-1] | lstrip('\n')", 
@@ -92,6 +86,14 @@ impl ChatTemplate {
         env.add_filter("rstrip", |s: String, chars: Option<String>| match chars {
             Some(chars_str) => s.trim_end_matches(chars_str.as_str()).to_string(),
             None => s.trim_end().to_string(),
+        });
+
+        env.add_test("startingwith", |s: String, prefix: String| {
+            s.starts_with(&prefix)
+        });
+
+        env.add_test("endingwith", |s: String, suffix: String| {
+            s.ends_with(&suffix)
         });
         
         let _ = env.add_template("chat", template);
