bcebd6ab449cbc015b12f3ea2952229f9499d1a0
oauth-network <127634415+oauth-network@users.noreply.github.com>
2026-03-12 02:38:36 +0900

    Update tensor_utils.rs

--- commerce/rust/src-tauri/src/utils/tensor_utils.rs
+++ commerce/rust/src-tauri/src/utils/tensor_utils.rs
@@ -36,12 +36,10 @@ pub fn repeat_kv(xs: Tensor, n_rep: usize) -> Result<Tensor> {
         Ok(xs)
     } else {
         let (b_sz, n_kv_head, seq_len, head_dim) = xs.dims4()?;
-        let kv = Tensor::cat(&vec![&xs; n_rep], 2)?.reshape((
-            b_sz,
-            n_kv_head * n_rep,
-            seq_len,
-            head_dim,
-        ))?;
+        let kv = xs
+            .unsqueeze(2)?
+            .expand((b_sz, n_kv_head, n_rep, seq_len, head_dim))?
+            .reshape((b_sz, n_kv_head * n_rep, seq_len, head_dim))?;
         Ok(kv)
     }
 }
