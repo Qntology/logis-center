8cf3782f75f66361e30a3b477d78882edcffc6a3
oauth-network <127634415+oauth-network@users.noreply.github.com>
2026-03-13 19:05:21 +0900

    Delete run_src_tauri_test.bat

--- commerce/rust/run_src_tauri_test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 65001 > nul
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-cd src-tauri
-cargo run --release --bin test_qwen --features cuda
